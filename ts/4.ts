de412bb8abba5bbcfad1e47a766a63a49750ade3
tglide <26071571+TGlide@users.noreply.github.com>
2023-12-11 11:50:40 +0000

    optimize

--- ts/4.ts
+++ ts/4.ts
@@ -4,20 +4,20 @@ import { readlines } from "./helpers/readlines";
 const log = createLog(false);
 const lines = await readlines("./inputs/4.txt");
 
-let points = 0;
-lines.forEach((line, i) => {
+function getOwnWinning(line: string) {
   const [, numbers] = line.split(":");
   const [winning, owned] = numbers.split("|");
-
   const [winNums, ownNums] = [winning, owned].map((arr) =>
     arr.split(/\s+/g).filter((s) => s.length)
   );
 
-  const gamePoints = winNums.reduce((acc, cur) => {
-    if (!ownNums.includes(cur)) return acc;
-    const num = Number(cur);
-    return acc === 0 ? 1 : acc * 2;
-  }, 0);
+  return winNums.filter((n) => ownNums.includes(n));
+}
+
+let points = 0;
+lines.forEach((line, i) => {
+  const winning = getOwnWinning(line);
+  const gamePoints = winning.reduce((acc, cur) => (acc === 0 ? 1 : acc * 2), 0);
   points += gamePoints;
   log(`Game ${i + 1}:`, gamePoints, `points`);
 });
@@ -36,26 +36,20 @@ const cachedToCopy: {
 while (scratchboards.length) {
   const i = scratchboards.shift() as number;
   if (cachedToCopy[i]) {
+    // log(`Game ${i + 1}:`, cachedToCopy[i], "scratchcards to copy (cached)");
     scratchboards.push(...cachedToCopy[i]);
     totalScratchboards += cachedToCopy[i].length;
-    // log(`Game ${i + 1}:`, cachedToCopy[i], "scratchcards to copy (cached)");
     continue;
   }
 
   const line = lines[i];
+  const winning = getOwnWinning(line);
 
-  const [, numbers] = line.split(":");
-  const [winning, owned] = numbers.split("|");
-
-  const [winNums, ownNums] = [winning, owned].map((arr) =>
-    arr.split(/\s+/g).filter((s) => s.length)
-  );
-
-  const toCopy = winNums.reduce((acc, curr) => {
-    if (!ownNums.includes(curr)) return acc;
+  const toCopy = winning.reduce((acc, curr) => {
     const prev = acc.at(-1) ?? i;
     return [...acc, prev + 1];
   }, [] as number[]);
+
   log(`Game ${i + 1}:`, toCopy, "scratchcards to copy");
   scratchboards.push(...toCopy);
   totalScratchboards += toCopy.length;
